2a844171fc07ba00f4d836ff2f53bbff370de65a
bavtlt <a.bochenkov@gmail.com>
2022-01-30 13:34:44 +0400

    encryptFileWithKey

--- src/Cryptology.java
+++ src/Cryptology.java
@@ -1,57 +1,94 @@
 import java.io.IOException;
-import java.sql.SQLOutput;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Cryptology {
-    private static final String ALPHABET = "";
-    private static final String Key = "";
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ .,!:;?%0123456789";
+    private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        String menuPage1 = "Выберите действие, введя его номер:\n" +
-                "1. Зашифровать текст в файле с помощью ключа.\n" +
-                "2. Расшифровать текст в файле с помощью ключа.\n" +
-                "3. Расшифровать текст в файле методом перебора (брут-форс).\n" +
-                "4. Расшифровать текст в файле методом статического анализа.\n";
-        System.out.println(menuPage1);
+        String firstMenuPage = "Выберите действие, введя его номер:\n" +
+                               "1. Зашифровать текст в файле с помощью ключа.\n" +
+                               "2. Расшифровать текст в файле с помощью ключа.\n" +
+                               "3. Расшифровать текст в файле методом перебора (брут-форс).\n" +
+                               "4. Расшифровать текст в файле методом статического анализа.\n";
+        System.out.println(firstMenuPage);
 
         while (true){
 
-            Scanner scanner = new Scanner(System.in);
-            int menuItem = scanner.nextInt();
+            int menuItem = SCANNER.nextInt();
 
-            try {
-                if(menuItem == 1){
-                    encryptFile();
-                    System.out.println("Файл успешно зашифрован.");
-                    break;
-                }
-                else if(menuItem >= 2 || menuItem <= 4){
-                    decipherFile();
-                    System.out.println("Файл успешно расшифрован.");
-                    break;
-                }
-                else {
-                    System.out.println("Такого номера нет в меню.");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            if(menuItem == 1){
+                encryptFileWithKey();
+                break;
             }
-
+            else if(menuItem == 2){
+                decipherFileWithKey();
+                break;
+            }
+            else if(menuItem == 3){
+                decipherFileBruteForce();
+                break;
+            }
+            else if(menuItem == 4){
+                decipherFileStatisticalAnalysis();
+                break;
+            }
+            else if(menuItem == 5) {
+                System.exit(0);
+                break;
+            }
+            else {
+                System.out.println("Такого номера нет в меню.");
+            }
         }
     }
 
-    private static void encryptFile() throws IOException {
-        encryptFileWithKey();
+
+    private static void encryptFileWithKey() {
+        System.out.println("Введите полный путь до файла:");
+        String filePath = SCANNER.nextLine();
+        String fileText = getFileText(filePath);
+        System.out.println("Введите ключ от 0 до 20:");
+        int key = SCANNER.nextInt();
+        StringBuilder stringBuilder = new StringBuilder();
+        if (fileText != null) {
+            for (int i = 0; i < fileText.length(); i++) {
+                char symbolText = fileText.charAt(i);
+                int alphabetSymbolIndex = ALPHABET.indexOf(symbolText);
+                //вычисляем новый индекс с учетом что если введут ключ больше длины алфавита
+                int newAlphabetSymbolIndex = (alphabetSymbolIndex + key)%ALPHABET.length();
+                char encryptedSymbol = ALPHABET.charAt(newAlphabetSymbolIndex);
+                stringBuilder.append(encryptedSymbol);
+            }
+        }
+        writeNewTextToFile(stringBuilder.toString(), filePath);
     }
 
-    private static void decipherFile() throws IOException {
-        decipherFileStatisticalAnalysis();
-        decipherFileBruteForce();
-        decipherFileWithKey();
+    private static String getFileText(String filePath) {
+        try {
+            return new String(Files.readAllBytes(Path.of(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    private static void decipherFileStatisticalAnalysis() {
+    private static void writeNewTextToFile(String text, String filePath) {
+        int dotIndex = filePath.lastIndexOf(".");
+        String pathBeforeDot = filePath.substring(0, dotIndex);
+        String pathAfterDot = filePath.substring(dotIndex);
+        String newFilePath = pathBeforeDot + "-encrypted" + pathAfterDot;
+        try {
+            Files.writeString(Path.of(newFilePath), text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+        private static void decipherFileStatisticalAnalysis() {
     }
 
     private static void decipherFileBruteForce() {
@@ -60,7 +97,5 @@ public class Cryptology {
     private static void decipherFileWithKey() {
     }
 
-    private static void encryptFileWithKey() {
-    }
 
 }
